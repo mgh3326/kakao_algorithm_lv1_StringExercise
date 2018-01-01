991f3fa25cb85fe0c12b5d45785b6ec5f0a6463e
mgh3326 <mgh3326@naver.com>
2018-01-01 16:27:06 +0900

    complete

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,11 +1,18 @@
-class StringExercise{
-    String getMiddle(String word){
-
-        return "";
+class StringExercise {
+    String getMiddle(String word) {
+        if (word.length() % 2 == 1)//홀수 일 경우
+        {
+            return word.substring(word.length() / 2, word.length() / 2 + 1);
+            //return Character.toString((word.charAt(word.length()/2)));
+        } else {
+            return word.substring(word.length() / 2-1, word.length() / 2 + 1);
+            //return Character.toString((word.charAt(word.length()/2-1))) + Character.toString((word.charAt(word.length()/2)));
+        }
     }
+
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
-    public static void  main(String[] args){
+    public static void main(String[] args) {
         StringExercise se = new StringExercise();
-        System.out.println(se.getMiddle("power"));
+        System.out.println(se.getMiddle("power1"));
     }
 }
